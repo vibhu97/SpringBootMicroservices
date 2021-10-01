0e7678f694a2361844797414a837903998cb446d
Administrator <Administrator@172.31.11.16>
2021-10-01 12:14:08 +0000

    Eureka Server

--- app01/src/main/java/com/vibhu/App01Application.java
+++ app01/src/main/java/com/vibhu/App01Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer  //eureka Server
+@EnableEurekaServer  
 public class App01Application {
 
 	public static void main(String[] args) {
